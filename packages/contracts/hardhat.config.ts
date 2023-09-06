4a8a76f2ce830f41d78a4766d604140eb47761fc
Lucas <44002091+CostierLucas@users.noreply.github.com>
2023-09-06 05:10:35 +0200

    Update hardhat.config.ts

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -12,20 +12,11 @@ const config: HardhatUserConfig = {
       },
     },
     goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/Gfs_EBL4dk84SzNvVXCCu0TvrntC-fwG',
-      accounts: [
-        '9d10ce0ae208c1b83dbb46d8f0149cddf5aea05616958f4d616b75637af77ca9',
-      ],
+      url: 'https://eth-goerli.g.alchemy.com/v2/Gfs_EBL4dk84SzNvVXCCu0TvrntC-fwG'
     },
     mainnet: {
-      url: 'https://eth-mainnet.g.alchemy.com/v2/SG8sbrNEK-FIi3MuEhAnAbUzgUoueexE',
-      accounts: [
-        'add94d36d64d87316eafe8e52a9d8e0f5754bb9d92b1ae9c9fa9e9b0a471f7fd',
-      ],
+      url: 'https://eth-mainnet.g.alchemy.com/v2/SG8sbrNEK-FIi3MuEhAnAbUzgUoueexE'
     },
-  },
-  etherscan: {
-    apiKey: 'RIWTSE5WFK3QXFN254IIQ5PF3AF9TXA28G',
   },
 };
 
