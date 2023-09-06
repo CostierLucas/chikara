3953d842ade48030b81905e76d32475a6cf46b59
Lucas <44002091+CostierLucas@users.noreply.github.com>
2023-09-06 05:10:54 +0200

    Update hardhat.config.ts

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -6,17 +6,6 @@ import '@nomiclabs/hardhat-etherscan';
 const config: HardhatUserConfig = {
   solidity: '0.8.19',
   networks: {
-    hardhat: {
-      forking: {
-        url: 'https://eth-mainnet.g.alchemy.com/v2/fmEJ3fFImEj9GkT4ezNuhCWPJGjItabq',
-      },
-    },
-    goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/Gfs_EBL4dk84SzNvVXCCu0TvrntC-fwG'
-    },
-    mainnet: {
-      url: 'https://eth-mainnet.g.alchemy.com/v2/SG8sbrNEK-FIi3MuEhAnAbUzgUoueexE'
-    },
   },
 };
 
